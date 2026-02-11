ed049b036607e3930a1fb61ec28d06f55a4f144d
sfrabby <fazlerabby6732@gmail.com>
2026-02-11 23:29:53 +0600

    Model added

---
+++ lib/Model/PostModel.dart
@@ -0,0 +1,26 @@
+class PostModel {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  PostModel({this.userId, this.id, this.title, this.body});
+
+  // JSON থেকে ডাটা মডেলে কনভার্ট করার জন্য
+  PostModel.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+  }
+
+  // মডেল থেকে পুনরায় JSON করার জন্য (প্রয়োজন হলে)
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['userId'] = userId;
+    data['id'] = id;
+    data['title'] = title;
+    data['body'] = body;
+    return data;
+  }
+}
