4335f0d90236dab53f5f86f1b5fb5993a119d1dc
sfrabby <fazlerabby6732@gmail.com>
2026-02-13 22:34:21 +0600

    Controller added

---
+++ lib/controller/DataPostController.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:get/get.dart';
+import '../Model/PostModel.dart'; // পাথ চেক করুন
+
+class PostController extends GetxController {
+  RxBool isLoading = false.obs;
+  RxList<PostModel> posts = <PostModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    isLoading.value = true;
+    try {
+      // URL এর শেষে /posts যোগ করা হয়েছে
+      var url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = jsonDecode(response.body);
+        posts.assignAll(jsonData.map((e) => PostModel.fromJson(e)).toList());
+        log("Fetched ${posts.length} posts");
+      }
+    } catch (e) {
+      log("Error: $e");
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
